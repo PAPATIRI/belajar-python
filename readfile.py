92ae5f649a1fbaa36e18743cd67894faa4264666
PAPATIRI <syariftaufik11@gmail.com>
2019-03-14 23:30:42 +0700

    cara membaca isi file pada python

---
+++ readfile.py
@@ -0,0 +1,5 @@
+baca = open("contohfile.txt","r").read()
+#readlines() -> membaca isi file dan ditampilkan dalam bentuk list
+#read() -> membaca semua isi dalam file tersebut
+#readline() -> membaca bagian awal/ line awal dari file tersebut, bila dalam kurung diisi nominal angka maka itu membaca isi file sebanyak nominal tersebut
+print(baca)
