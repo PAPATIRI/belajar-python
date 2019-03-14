9881781cdaae8ea18aa9aa07e35d8510572422e7
PAPATIRI <syariftaufik11@gmail.com>
2019-03-14 23:42:57 +0700

    class sederhana pada python

---
+++ class.py
@@ -0,0 +1,19 @@
+class kalkulator:
+    def pertambahan(a,b):
+        hasil=a+b
+        print(hasil)
+    def pengurangan(a,b):
+        hasil=a-b
+        print(hasil)
+    def perkalian(x,y):
+        hasil=x*y
+        print(hasil)
+    def pembagian(x,y):
+        hasil=x/y
+        print(hasil)
+
+#pemanggilan method" dalam kelas sebagai berikut
+kalkulator.pertambahan(2,1)
+kalkulator.pengurangan(2,1)
+kalkulator.perkalian(2,3)
+kalkulator.pembagian(2,1)
