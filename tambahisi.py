ad34ae1d6e5d6a40e85e5ea386806533876a1092
PAPATIRI <syariftaufik11@gmail.com>
2019-03-14 23:22:03 +0700

    cara menambah isi pada file yang sudah dibuat

---
+++ tambahisi.py
@@ -0,0 +1,5 @@
+tambahisi = "\nini hasil dari tambah isi pada file(append file on python)"
+
+appendFile = open("contohfile.txt","a") #a untuk perintah append(tambah)
+appendFile.write(tambahisi)
+appendFile.close()
